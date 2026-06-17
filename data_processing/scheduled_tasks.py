aa8ed40665a6afbc211868113c94f381fb1f192f
Adam Dracz <adam.dracz@gmail.com>
2026-06-17 04:35:20 +0200

    dashboard and schedule

--- data_processing/scheduled_tasks.py
+++ data_processing/scheduled_tasks.py
@@ -6,6 +6,7 @@ from database_connection.conn import *
 import datetime
 import time
 
+# regularne pobieranie danych z API
 def download_and_insert_to_db():
 
     date = datetime.today().date()
@@ -17,6 +18,31 @@ def download_and_insert_to_db():
 
 # wg strony NBP dane są udostępniane między 11.45 a 12.15 - dodany jest jeszcze dodatkowy bufor czasowy do 13.30
 schedule.every().day.at("13:30").do(download_and_insert_to_db)
+
+
+# trenowanie modeli
+def model_training():
+
+    df = get_data_from_db()
+
+    # xgboost
+    models_dict, best_params_dict, cv_scores, test_scores_dict = get_ml_models_and_scores(df, 
+        curr_models_dict = None, model_type = "xgboost")
+
+    save_models_data_to_db(best_params_dict, cv_scores_dict, test_scores_dict, "xgboost")
+
+    # arima
+    models_dict, best_params_dict, cv_scores, test_scores_dict = get_ml_models_and_scores(df, 
+    curr_models_dict = None, model_type = "arima")
+
+    save_models_data_to_db(best_params_dict, cv_scores_dict, test_scores_dict, "arima")
+
+
+# trenowanie co 2 tygodnie
+schedule.every(2).weeks.do(model_training)
+
+
+
 
 
 while True:
